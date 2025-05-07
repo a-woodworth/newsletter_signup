7b2013dd5d5270e23834c683fcffcb3f52a579b7
Amy Woodworth <woodworthcommunications@gmail.com>
2025-05-07 12:32:34 -0500

    Fix style issues. Add js for email validation.

---
+++ app.js
@@ -0,0 +1,79 @@
+const signupDiv = document.querySelector('.signup');
+const successDiv = document.querySelector('.wrapper--success');
+const signupForm = document.querySelector('.newsletter__form');
+const emailInput = signupForm.elements.email;
+const emailError = document.querySelector('#email-error');
+const successEmail = document.querySelector('.success__email');
+const dismissBtn = successDiv.querySelector('.btn');
+
+function handleSuccessMessageExit() {
+  // Hide success message
+  successDiv.classList.add('hidden');
+  // Show signup form & reset form
+  signupDiv.classList.remove('hidden');
+  signupForm.reset();
+  // Add aria atribute to h1 so screen readers know page has changed
+  signupDiv.querySelector('h1').setAttribute('aria-live', 'polite');
+}
+
+function removeEmailError() {
+  // Update aria attributes
+  emailInput.setAttribute('aria-invalid', 'false');
+  emailInput.removeAttribute('aria-live', 'polite');
+
+  // Show error message
+  emailError.classList.add('hidden');
+}
+
+function showEmailError() {
+  // Update aria attributes
+  emailInput.setAttribute('aria-invalid', 'true');
+  emailInput.setAttribute('aria-live', 'polite');
+
+  // Show error message
+  emailError.classList.remove('hidden');
+}
+
+function validateForm(e) {
+  e.preventDefault();
+  const validForm = signupForm.checkValidity();
+
+  if (!validForm) {
+    // Email input is invalid - cancel submit and show error
+    showEmailError();
+    emailInput.focus();
+  } else {
+    // Show success message
+    signupDiv.classList.add('hidden');
+    successEmail.textContent = emailInput.value;
+    successDiv.classList.remove('hidden');
+  }
+}
+
+// Show email error when exiting input
+emailInput.addEventListener('blur', () => {
+  const validEmail = emailInput.checkValidity();
+
+  if (!validEmail) {
+    showEmailError();
+  }
+});
+
+// Check for email error correction
+emailInput.addEventListener('input', () => {
+  const validEmail = emailInput.checkValidity();
+
+  if (validEmail) {
+    removeEmailError();
+  }
+});
+
+// Validate form on submit
+signupForm.addEventListener('submit', (e) => {
+  validateForm(e);
+});
+
+// Dismiss success message
+dismissBtn.addEventListener('click', () => {
+  handleSuccessMessageExit();
+});
